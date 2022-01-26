98bd1b77c2d8dd975b2f0d56526b5b94b6ac3dec
Petar Paar <pepaar@microsoft.com>
2022-01-26 23:55:16 +0100

    Add depth

--- src/components/orderBook/orderLine/OrderLine.tsx
+++ src/components/orderBook/orderLine/OrderLine.tsx
@@ -11,25 +11,30 @@ interface Props {
   priceColor: PriceColor;
 }
 
-export const OrderLine: React.FC<Props> = ({ order, isLeftAligned, priceColor }) => {
+export const OrderLine: React.FC<Props> = (props) => {
+  const levelDepth = (props.order.total / props.highestTotal) * 100;
+  const bgColor = priceColorToBackgroundColor(props.priceColor);
+  const lineBackgroundStyle = `linear-gradient(to left, ${bgColor} ${levelDepth}%, transparent ${levelDepth}%)`;
+
   return (
-    <>
-      <div className={priceTextStyles(priceColor)}>{formatNumericValue(order.price)}</div>
-      <div className={styles.normalText}>{formatNumericValue(order.size)}</div>
-      <div className={styles.normalText}>{formatNumericValue(order.total)}</div>
-    </>
+    <div className={styles.row} style={{ background: lineBackgroundStyle }}>
+      <span className={priceTextStyles(props.priceColor)}>{formatNumericValue(props.order.price)}</span>
+      <span className={styles.normalText}>{formatNumericValue(props.order.size)}</span>
+      <span className={styles.normalText}>{formatNumericValue(props.order.total)}</span>
+    </div>
   );
 };
 
 export const OrderLineTitle: React.FC<{ isLeftAligned: boolean }> = ({ isLeftAligned }) => {
   return (
-    <>
-      <div className={styles.titleText}>PRICE</div>
-      <div className={styles.titleText}>SIZE</div>
-      <div className={styles.titleText}>TOTAL</div>
-    </>
+    <div className={styles.row}>
+      <span className={styles.titleText}>PRICE</span>
+      <span className={styles.titleText}>SIZE</span>
+      <span className={styles.titleText}>TOTAL</span>
+    </div>
   );
 };
 
 const priceTextStyles = (price: PriceColor) => (price === "green" ? styles.greenText : styles.redText);
+const priceColorToBackgroundColor = (price: PriceColor) => (price === "green" ? "#4ba54b40" : "#b911116e");
 const formatNumericValue = (value: number) => value.toLocaleString("en-us");
