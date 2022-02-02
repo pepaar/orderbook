c1f488aad30759822e6f27c857fc3b2f3d7ad58b
Petar Paar <pepaar@microsoft.com>
2022-02-02 20:20:44 +0100

    Fix for toggle

--- src/components/orderBook/toogleFeed/ToggleFeed.tsx
+++ src/components/orderBook/toogleFeed/ToggleFeed.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { Currency, selectedCurrencyState } from "../../../state/orderBook";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Currency, selectedCurrencyState, topOfOrderBookState } from "../../../state/orderBook";
 import { Button } from "../../common/button/Button";
 import styles from "./ToggleFeed.module.css";
 
 export const ToggleFeed: React.FC = () => {
   const [currency, setCurrency] = useRecoilState(selectedCurrencyState);
+  const orderBook = useRecoilValue(topOfOrderBookState);
 
   const toggleCurrency = React.useCallback(() => {
     const newCurrency: Currency = currency === "Bitcoin" ? "Ethereum" : "Bitcoin";
     setCurrency(newCurrency);
   }, [currency, setCurrency]);
+
+  if (!orderBook) {
+    return null;
+  }
 
   return (
     <footer className={styles.footer}>
